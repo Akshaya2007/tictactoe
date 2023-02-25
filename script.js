122aa22f53d1428c24ed17197d0a4c318fbbd188
Akshaya2007 <81800055+Akshaya2007@users.noreply.github.com>
2023-02-25 10:46:55 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,64 @@
 document.addEventListener("DOMContentLoaded", function() {
   // get elements
-  var startButton = document.getElementById("start-button");
-  var resetButton = document.getElementById("reset-button");
-  var homePage = document.getElementById("home-page");
-  var gamePage = document.getElementById("game-page");
   var gameBoard = document.getElementById("game-board");
 
-  // add event listeners
-  startButton.addEventListener("click", function() {
-    homePage.style.display = "none";
-    gamePage.style.display = "block";
-  });
+  // initialize variables
+  var currentPlayer = "X";
+  var moves = 0;
 
-  resetButton.addEventListener("click", function() {
-    // reset the game board
-    for (var i = 0; i < 9; i++) {
-      document.getElementById(i.toString()).innerHTML = "";
-    }
-
-    // show the game board
-    gamePage.style.display = "block";
-  });
-
+  // add event listener to game board
   gameBoard.addEventListener("click", function(event) {
     // get the clicked cell
     var cell = event.target;
 
     // check if the cell is empty
     if (cell.innerHTML === "") {
-      // get the current player
-      var currentPlayer = document.getElementById("current-player").innerHTML;
-
       // update the cell with the current player's symbol
       cell.innerHTML = currentPlayer;
 
       // check for a win
       if (checkWin()) {
         alert(currentPlayer + " wins!");
-        gamePage.style.display = "none";
+        reset();
       } else {
         // switch players
         if (currentPlayer === "X") {
-          document.getElementById("current-player").innerHTML = "O";
+          currentPlayer = "O";
         } else {
-          document.getElementById("current-player").
+          currentPlayer = "X";
+        }
+
+        // increment moves counter
+        moves++;
+
+        // check for a tie
+        if (moves === 9) {
+          alert("Tie game!");
+          reset();
+        }
+      }
+    }
+  });
+
+  function checkWin() {
+    // check rows
+    for (var i = 0; i < 9; i += 3) {
+      if (document.getElementById(i.toString()).innerHTML !== "" &&
+          document.getElementById(i.toString()).innerHTML === document.getElementById((i+1).toString()).innerHTML &&
+          document.getElementById((i+1).toString()).innerHTML === document.getElementById((i+2).toString()).innerHTML) {
+        return true;
+      }
+    }
+
+    // check columns
+    for (var i = 0; i < 3; i++) {
+      if (document.getElementById(i.toString()).innerHTML !== "" &&
+          document.getElementById(i.toString()).innerHTML === document.getElementById((i+3).toString()).innerHTML &&
+          document.getElementById((i+3).toString()).innerHTML === document.getElementById((i+6).toString()).innerHTML) {
+        return true;
+      }
+    }
+
+    // check diagonals
+    if (document.getElementById("0").innerHTML !== "" &&
+        document.getElementById("0").innerHTML === document.getElementById("4").innerHTML &&
